99a1745cf65294d4057afe1673ba201b0d0d002e
lilian <lilianchidinmalivinus@gmail.com>
2025-09-09 15:45:09 +0100

    updated file

--- sript.js
+++ sript.js
@@ -24,14 +24,56 @@ console.log(greet("Faith")); // Output: Hello Faith!
 }
 
 const container = document.getElementById("container");
-  container.innerHTML = "<p>JavaScript Indept Knowledge</p>"; // Adds a paragraph by replacing the previous
-  console.log(container.innerHTML)
+  container.innerHTML = "<p>JavaScript Indept Knowledge</p>"; // Adds a paragraph by replacing the previousconsole.log(container.innerHTML)
 
 
-  const h1 = document.querySelector("h1"); // first h1
-  console.log(h1.textContent);
+const h1 = document.querySelector("h1"); // first h1
+    console.log(h1.textContent);
 
-  const intro = document.getElementById("intro"); // show content
-  console.log(intro.innerText);
+const intro = document.getElementById("intro"); // show content
+    console.log(intro.innerText);
 
 
+const form = document.getElementById("myDetail");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // stop page reload
+
+    // Get form values
+    const name = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+
+    // Display result
+    result.textContent = `Form submitted! Name: ${name}, Email: ${email}`;
+});
+
+ //  click the button for alert pop up
+const btn = document.getElementById("btnSubmit");
+
+btn.addEventListener("click", () => {
+    alert("Button was clicked!");
+
+    }
+);
+
+// change the color of text on mouseover
+const text = document.getElementById("hoverText");
+
+text.onmouseover = function () {
+    text.style.color = "blue";  // mouse over text, color changes to blue
+    text.style.fontWeight = "bold";
+};
+
+text.onmouseout = function () {
+    text.style.color = "black";    // when mouse is out, text color is black
+    text.style.fontWeight = "normal";
+};
+
+// to open link in a new tab
+const myLink = document.getElementById("link");
+
+myLink.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    window.open(myLink.href, "_blank"); // Open in new tab
+});
